b53135398e1ae9f3d459ad4065ca5918ab4497ae
valmet <sayaka_nakamura@r.recruit.co.jp>
2020-07-02 23:10:01 +0900

    use DDLog in ViewController

--- EmbeddedFramework/ViewController.m
+++ EmbeddedFramework/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [DDLog addLogger:[DDOSLogger sharedInstance]]; // Uses os_log
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
