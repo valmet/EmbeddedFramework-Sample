c461c3427688a704bd9db3d5edcb09b0d6b9a04f
valmet <sayaka_nakamura@r.recruit.co.jp>
2020-07-02 23:11:06 +0900

    add Logger target

---
+++ LoggerTests/LoggerTests.m
@@ -0,0 +1,37 @@
+//
+//  LoggerTests.m
+//  LoggerTests
+//
+//  Created by 中村　彩香 on 2020/07/02.
+//  Copyright © 2020 valmet. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoggerTests : XCTestCase
+
+@end
+
+@implementation LoggerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
