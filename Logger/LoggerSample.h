676b4ea6e7b8442baeddbb089d73e93dab0f23d6
valmet <sayaka_nakamura@r.recruit.co.jp>
2020-07-02 23:12:24 +0900

    add Logger Sample

---
+++ Logger/LoggerSample.h
@@ -0,0 +1,17 @@
+//
+//  LoggerSample.h
+//  EmbeddedFramework
+//
+//  Created by 中村　彩香 on 2020/07/02.
+//  Copyright © 2020 valmet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoggerSample : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Logger/LoggerSample.m
@@ -0,0 +1,13 @@
+//
+//  LoggerSample.m
+//  EmbeddedFramework
+//
+//  Created by 中村　彩香 on 2020/07/02.
+//  Copyright © 2020 valmet. All rights reserved.
+//
+
+#import "LoggerSample.h"
+
+@implementation LoggerSample
+
+@end
